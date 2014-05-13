f0a2140d7a6480f0075cc04cd78701b8781b9c42
BramRutten <rutten_bram@hotmaail.com>
2014-05-13 21:50:40 +0200

    Beginfiles + Werkende Google Map Api

---
+++ user.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Wheelable - user</title>
+</head>
+<body>
+
+
+
+<?php 
+
+include 'Maps/api.php';
+
+
+?>	
+</body>
+</html>
